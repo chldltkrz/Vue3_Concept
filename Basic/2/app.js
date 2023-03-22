429fa42e1eabe5ef47ae35995a4e31feef5fb1f6
Issac Choi <chldltkrz@gmail.com>
2023-03-22 11:10:28 +0900

    added event related concept and challenge file
    Signed-off-by: Issac Choi <chldltkrz@gmail.com>

--- Basic/2/app.js
+++ Basic/2/app.js
@@ -2,8 +2,29 @@ const app = Vue.createApp({
   data() {
     return {
       counter: 0,
+      name: "",
+      confirmedName: "",
     };
+  },
+  methods: {
+    confirmInput() {
+      this.confirmedName = this.name;
+    },
+    submitForm(event) {
+      event.preventDefault();
+      alert("Submitted!");
+    },
+    setName(e) {
+      this.name = e.target.value;
+    },
+    add() {
+      this.counter++;
+    },
+
+    reduce() {
+      this.counter--;
+    },
   },
 });
 
-app.mount('#events');
+app.mount("#events");
