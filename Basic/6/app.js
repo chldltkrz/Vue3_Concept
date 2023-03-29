2ac18c4c16cba97e41d5f0ba93b885f1b6e3d1c9
Issac Choi <chldltkrz@gmail.com>
2023-03-29 13:57:46 +0900

    wrote some life cycle contents
    Signed-off-by: Issac Choi <chldltkrz@gmail.com>

--- Basic/6/app.js
+++ Basic/6/app.js
@@ -1,8 +1,13 @@
 const app = Vue.createApp({
+  // if empty html element, it templete will be shown
+  templete: `
+    <p> Hi there </p>
+    <p> {{message}} </p>
+  `,
   data() {
     return {
-      currentUserInput: '',
-      message: 'Vue is great!',
+      currentUserInput: "",
+      message: "Vue is great!",
     };
   },
   methods: {
@@ -10,9 +15,68 @@ const app = Vue.createApp({
       this.currentUserInput = event.target.value;
     },
     setText() {
-      this.message = this.currentUserInput;
+      // this.message = this.currentUserInput;
+      this.message = this.$refs.userText.value;
+    },
+    beforeCreate() {
+      // See through the break point on browser
+      console.log("beforeCreate()");
+    },
+    created() {
+      console.log("created()");
+    },
+    beforeMount() {
+      console.log("beforeMount()");
+    },
+    mounted() {
+      console.log("Mounted()");
+    },
+    beforeUpdate() {
+      console.warn("beforeUpdate()");
+    },
+    updated() {
+      console.warn("updated()");
+    },
+    beforeUnmount() {
+      console.error("beforeUnmount()");
+    },
+    unmounted() {
+      console.errpr("unmounted()");
     },
   },
 });
 
-app.mount('#app');
+// Vue can have more than one app!
+// each vue app is independent to each other!!
+//
+app.mount("#app");
+
+// Proof that JS is not reactive,
+// long message is not changed after modification
+let message = "Hello";
+let longMessage = message + " world!";
+console.log(longMessage);
+message = "HElOEoe!!!!!";
+console.log(longMessage);
+
+// JS Proxy, Vue is working kinda similar way,
+// Vue wraps all the data in proxy!
+const data = {
+  message: "hello!",
+  longMessage: "Hello! World!",
+};
+const handler = {
+  set(target, key, value) {
+    // console.log(target);
+    // console.log(key);
+    // console.log(value);
+    if (key === "message") {
+      target.longMessage = value + " World!!";
+    }
+    target.message = value;
+  },
+};
+const proxy = new Proxy(data, handler);
+proxy.message = "Hello!!!!!";
+console.log(proxy.longMessage);
+// app.unmount();
