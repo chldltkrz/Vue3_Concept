ef51f0a4c2766171774ebe714fef0b18e885e897
Issac Choi <chldltkrz@gmail.com>
2023-03-29 16:25:08 +0900

    super basic usage of the component!
    Signed-off-by: Issac Choi <chldltkrz@gmail.com>

--- Basic/7(Component)/app.js
+++ Basic/7(Component)/app.js
@@ -1,7 +1,6 @@
 const app = Vue.createApp({
   data() {
     return {
-      detailsAreVisible: false,
       friends: [
         {
           id: "manual",
@@ -18,9 +17,42 @@ const app = Vue.createApp({
       ],
     };
   },
+});
+// component is like CUSTOM HTML Code!
+// need to make html tag that contains dash(-) and two words[rule of thumb]
+// second argument is also app, but it is more likely sidecar app that connected to the main app
+// needs also the template to apply!
+app.component("friend-contact", {
+  template: `
+         <li>
+          <h2>{{ friend.name }}</h2>
+          <!-- !!!PROBLEM!!! 
+                that if button is clicked, all friends' Detail is Open!!
+          -->
+          <button @click="toggleDetails()">
+            {{ detailsAreVisible ? 'Hide' : 'Show' }}
+          </button>
+          <ul v-if="detailsAreVisible">
+            <li><strong>Phone:</strong> {{ friend.phone }}</li>
+            <li><strong>Email:</strong> {{ friend.email }}</li>
+          </ul>
+        </li>
+  `,
+  data() {
+    return {
+      detailsAreVisible: false,
+      friend: {
+        id: "manual",
+        name: "manual",
+        phone: "01234 51203 2039",
+        email: "manuel@.localhost.com",
+      },
+    };
+  },
   methods: {
     toggleDetails() {
       this.detailsAreVisible = !this.detailsAreVisible;
     },
   },
-}).mount("#app");
+});
+app.mount("#app");
