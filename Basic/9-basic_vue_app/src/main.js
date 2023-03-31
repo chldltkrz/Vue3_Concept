bc426cd4fb9035a5b7caff167a6aeb2edf0a11ef
Issac Choi <chldltkrz@gmail.com>
2023-03-31 10:28:00 +0900

    added first Vue Cli dummy project
    Signed-off-by: Issac Choi <chldltkrz@gmail.com>

--- Basic/9-basic_vue_app/src/main.js
+++ Basic/9-basic_vue_app/src/main.js
@@ -1,3 +1,7 @@
-import { createApp } from 'vue';
+import { createApp } from "vue";
+import App from "./App.vue";
+import FriendContact from "./components/FriendContact.vue";
+const app = createApp(App);
 
-createApp({}).mount('');
+app.component("friend-contact", FriendContact);
+app.mount("#app");
