144eb2a43a65bfacf52b788c08d2123ebc6c3c60
Issac Choi <chldltkrz@gmail.com>
2023-04-05 17:51:00 +0900

    added some openapi testing codes
    Signed-off-by: Issac Choi <chldltkrz@gmail.com>

--- Project/chatvue/src/App.vue
+++ Project/chatvue/src/App.vue
@@ -10,6 +10,34 @@ import PageHeader from "./components/header/PageHeader.vue";
 import TextField from "./components/body/TextField.vue";
 import InputField from "./components/body/InputField.vue";
 import PageFooter from "./components/footer/PageFooter.vue";
+
+const OpenAI = require("openai-api");
+
+// Load your key from an environment variable or secret management service
+// (do not include your key directly in your code)
+// const OPENAI_API_KEY = process.env.OPENAI_API_KEY;
+const OPENAI_API_KEY = "";
+
+const openai = new OpenAI(OPENAI_API_KEY);
+
+(async () => {
+  const gptResponse = await openai.complete({
+    engine: "davinci",
+    prompt: "this is a test",
+    maxTokens: 5,
+    temperature: 0.9,
+    topP: 1,
+    presencePenalty: 0,
+    frequencyPenalty: 0,
+    bestOf: 1,
+    n: 1,
+    stream: false,
+    stop: ["\n", "testing"],
+  });
+
+  console.log(gptResponse.data);
+})();
+
 export default {
   name: "App",
   components: { PageHeader, PageFooter, TextField, InputField },
