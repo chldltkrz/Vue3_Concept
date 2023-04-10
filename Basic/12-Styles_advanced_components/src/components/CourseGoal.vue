3da8f19811e1192165ff4a810fc4ad4fd7df6888
Issac Choi <chldltkrz@gmail.com>
2023-04-10 15:53:38 +0900

    added scoped slot example
    Signed-off-by: Issac Choi <chldltkrz@gmail.com>

---
+++ Basic/12-Styles_advanced_components/src/components/CourseGoal.vue
@@ -0,0 +1,19 @@
+<template>
+  <ul>
+    <!-- if have following code, then if want to access the props -->
+    <li v-for="goal in goals" :key="goal">
+      <!-- if bind it to the some specific value, -> refer to App.vue -->
+      <slot :item="goal" another-props="..."></slot>
+    </li>
+  </ul>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      goals: ["finished the course", "Learn Vue"],
+    };
+  },
+};
+</script>
