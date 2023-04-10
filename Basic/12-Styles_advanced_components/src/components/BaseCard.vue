b2394ff2a22626b292d9e1e9c37dcfb25129c279
Issac Choi <chldltkrz@gmail.com>
2023-04-10 14:13:10 +0900

    added more usage on slot concept
    Signed-off-by: Issac Choi <chldltkrz@gmail.com>

--- Basic/12-Styles_advanced_components/src/components/BaseCard.vue
+++ Basic/12-Styles_advanced_components/src/components/BaseCard.vue
@@ -1,5 +1,15 @@
 <template>
   <div>
+    <!-- this slot element will be replaced with the parent html code! -->
+    <!-- if there is no name in the slot, that slot will be the default slot to dispaly -->
+    <!-- if there is name in the slot, matching html element will be placed in there -->
+    <header>
+      <slot name="header">
+        <h2>
+          This is default element if there is no content is passed to this slot
+        </h2>
+      </slot>
+    </header>
     <slot></slot>
   </div>
 </template>
@@ -7,6 +17,11 @@
 export default {};
 </script>
 <style scoped>
+section div {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
 div {
   margin: 2rem auto;
   max-width: 30rem;
--- Basic/12-Styles_advanced_components/src/components/UserInfo.vue
+++ Basic/12-Styles_advanced_components/src/components/UserInfo.vue
@@ -1,10 +1,14 @@
 <template>
   <section>
     <base-card>
-      <div>
+      <!-- in order to use named slot feature name of element of wrapper 
+          should be the "template with v-slot:[argument]"
+      -->
+      <!-- <template #header> -->
+      <template v-slot:header>
         <h3>{{ fullName }}</h3>
         <base-badge :type="role" :caption="role.toUpperCase()"></base-badge>
-      </div>
+      </template>
       <p>{{ infoText }}</p>
     </base-card>
   </section>
@@ -15,19 +19,22 @@ export default {
   props: ["fullName", "infoText", "role"],
 };
 </script>
-
+<!-- since the css code in the file will be compiled before the markup is sent to the slot, 
+      All the css should be moved to the Slot wrapper file!
+-->
+<!-- 
 <style>
-section {
+/* section {
   margin: 2rem auto;
   max-width: 30rem;
   border-radius: 12px;
   box-shadow: 0 2px 8px rgba(0, 0, 0, 0.26);
   padding: 1rem;
-}
+} */
 
 section div {
   display: flex;
   justify-content: space-between;
   align-items: center;
 }
-</style>
+</style> -->
