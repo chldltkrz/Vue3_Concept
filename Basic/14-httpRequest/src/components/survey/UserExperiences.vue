d7aab753bc70cb8ed2a5401684edde0e8678dbc6
Issac Choi <chldltkrz@gmail.com>
2023-04-14 13:26:43 +0900

    added get call by fetch method from the page
    Signed-off-by: Issac Choi <chldltkrz@gmail.com>

--- Basic/14-httpRequest/src/components/survey/UserExperiences.vue
+++ Basic/14-httpRequest/src/components/survey/UserExperiences.vue
@@ -3,7 +3,9 @@
     <base-card>
       <h2>Submitted Experiences</h2>
       <div>
-        <base-button>Load Submitted Experiences</base-button>
+        <base-button @click="loadExperiences"
+          >Load Submitted Experiences</base-button
+        >
       </div>
       <ul>
         <survey-result
@@ -21,9 +23,37 @@
 import SurveyResult from './SurveyResult.vue';
 
 export default {
-  props: ['results'],
   components: {
     SurveyResult,
+  },
+  data() {
+    return {
+      results: [],
+    };
+  },
+  methods: {
+    loadExperiences() {
+      fetch(
+        'https://vue-concept-httprequest-default-rtdb.asia-southeast1.firebasedatabase.app/surveys.json'
+      )
+        .then((response) => {
+          if (response.ok) {
+            return response.json();
+          }
+        })
+        // function() create this local, but arrow function will create this refer to outside the function
+        .then((data) => {
+          const result = [];
+          for (const id in data) {
+            result.push({
+              id: id,
+              name: data[id].name,
+              rating: data[id].rating,
+            });
+          }
+          this.results = result;
+        });
+    },
   },
 };
 </script>
