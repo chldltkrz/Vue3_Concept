de0c1095b2247af012a1da31c89643220f18742f
Issac Choi <chldltkrz@gmail.com>
2023-05-03 11:13:32 +0900

    deleting not necessary files and create new files
    Signed-off-by: Issac Choi <chldltkrz@gmail.com>

--- Project/coachfinder/src/router/index.js
+++ Project/coachfinder/src/router/index.js
@@ -1,20 +1,37 @@
 import { createRouter, createWebHistory } from "vue-router";
-import HomeView from "../views/HomeView.vue";
+
+import CoachDetails from "../pages/coaches/CoachDetails.vue";
+import CoachesList from "../pages/coaches/CoachesList.vue";
+import CoachRegister from "../pages/coaches/CoachRegister.vue";
+import ContactCoach from "../pages/requests/ContactCoach.vue";
+import RequestsReceived from "../pages/requests/RequestsReceived.vue";
+import NotFound from "../pages/NotFound.vue";
 
 const routes = [
   {
     path: "/",
-    name: "home",
-    component: HomeView,
+    redirect: "/coaches",
   },
   {
-    path: "/about",
-    name: "about",
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () =>
-      import(/* webpackChunkName: "about" */ "../views/AboutView.vue"),
+    path: "/coaches",
+    component: CoachesList,
+  },
+  {
+    path: "/coaches/:id",
+    component: CoachDetails,
+    children: [{ path: "contact", component: ContactCoach }],
+  },
+  {
+    path: "/register",
+    component: CoachRegister,
+  },
+  {
+    path: "/requests",
+    component: RequestsReceived,
+  },
+  {
+    path: "/:notFound(.*)",
+    component: NotFound,
   },
 ];
 
