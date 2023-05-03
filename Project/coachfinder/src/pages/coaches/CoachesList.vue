d12646ac2df1d359de780e7db5709171b9cdb233
Issac Choi <chldltkrz@gmail.com>
2023-05-03 13:57:46 +0900

    added Coach data and Vuex codes
    Signed-off-by: Issac Choi <chldltkrz@gmail.com>

--- Project/coachfinder/src/pages/coaches/CoachesList.vue
+++ Project/coachfinder/src/pages/coaches/CoachesList.vue
@@ -5,8 +5,23 @@
       <button>Refresh</button>
       <router-link to="register">Register as Coach</router-link>
     </div>
-    <ul>
-      LIST
+    <ul v-if="hasCoaches">
+      <li v-for="c in filteredCoaches" :key="c.id">
+        {{ c.firstName }}
+      </li>
     </ul>
+    <h3 v-else>Currently has no coache Available</h3>
   </section>
 </template>
+<script>
+export default {
+  computed: {
+    filteredCoaches() {
+      return this.$store.getters["coaches/coaches"];
+    },
+    hasCoaches() {
+      return this.$store.getters["coaches/hasCoaches"];
+    },
+  },
+};
+</script>
--- Project/coachfinder/src/store/index.js
+++ Project/coachfinder/src/store/index.js
@@ -1,9 +1,13 @@
 import { createStore } from "vuex";
-
+import coaches from "./modules/coaches/index.js";
+import requests from "./modules/requests/index.js";
 export default createStore({
   state: {},
   getters: {},
   mutations: {},
   actions: {},
-  modules: {},
+  modules: {
+    coaches: coaches,
+    requests: requests,
+  },
 });
