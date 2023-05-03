f99bcbbf4f5ed13fc623fb98234eb4a8e03907fb
Issac Choi <chldltkrz@gmail.com>
2023-05-03 15:12:38 +0900

    added filters for filtering the coach in their area
    Signed-off-by: Issac Choi <chldltkrz@gmail.com>

--- Project/coachfinder/src/pages/coaches/CoachesList.vue
+++ Project/coachfinder/src/pages/coaches/CoachesList.vue
@@ -1,5 +1,7 @@
 <template>
-  <section>Filter</section>
+  <section>
+    <coach-filter @change-filter="setFilter"></coach-filter>
+  </section>
   <section>
     <base-card>
       <div class="controls">
@@ -24,18 +26,47 @@
 </template>
 <script>
 import CoachItem from "../../components/coaches/CoachItem.vue";
+import CoachFilter from "../../components/coaches/CoachFilter.vue";
 
 export default {
   components: {
     CoachItem,
+    CoachFilter,
   },
   computed: {
     filteredCoaches() {
-      return this.$store.getters["coaches/coaches"];
+      // return this.$store.getters["coaches/coaches"];
+      const coaches = this.$store.getters["coaches/coaches"];
+      return coaches.filter((coach) => {
+        if (this.activeFilters.frontend && coach.areas.includes("frontend")) {
+          return true;
+        }
+        if (this.activeFilters.backend && coach.areas.includes("backend")) {
+          return true;
+        }
+        if (this.activeFilters.career && coach.areas.includes("career")) {
+          return true;
+        }
+        return false;
+      });
     },
     hasCoaches() {
       return this.$store.getters["coaches/hasCoaches"];
     },
+  },
+  methods: {
+    setFilter(updatedFilters) {
+      this.activeFilters = updatedFilters;
+    },
+  },
+  data() {
+    return {
+      activeFilters: {
+        frontend: true,
+        backend: true,
+        career: true,
+      },
+    };
   },
 };
 </script>
