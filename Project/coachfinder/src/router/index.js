4053e94410837a4c99ed62c14a08ae4f86405949
Issac Choi <chldltkrz@gmail.com>
2023-05-31 14:32:08 +0900

    added coach registration form
    Signed-off-by: Issac Choi <chldltkrz@gmail.com>

--- Project/coachfinder/src/router/index.js
+++ Project/coachfinder/src/router/index.js
@@ -2,7 +2,7 @@ import { createRouter, createWebHistory } from "vue-router";
 
 import CoachDetails from "../pages/coaches/CoachDetails.vue";
 import CoachesList from "../pages/coaches/CoachesList.vue";
-import CoachRegister from "../pages/coaches/CoachRegister.vue";
+import CoachRegistration from "../pages/coaches/CoachRegistration.vue";
 import ContactCoach from "../pages/requests/ContactCoach.vue";
 import RequestsReceived from "../pages/requests/RequestsReceived.vue";
 import NotFound from "../pages/NotFound.vue";
@@ -24,7 +24,7 @@ const routes = [
   },
   {
     path: "/register",
-    component: CoachRegister,
+    component: CoachRegistration,
   },
   {
     path: "/requests",
