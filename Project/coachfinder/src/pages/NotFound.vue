42e46cc2913891032b0a68b91879debaad3b2486
Issac Choi <chldltkrz@gmail.com>
2023-06-14 16:01:34 +0900

    added page not found page
    Signed-off-by: Issac Choi <chldltkrz@gmail.com>

--- Project/coachfinder/src/pages/NotFound.vue
+++ Project/coachfinder/src/pages/NotFound.vue
@@ -1,3 +1,11 @@
 <template>
-  <div></div>
+  <div>
+    <base-card>
+      <h2>Page not found</h2>
+      <p>
+        this page cannot found..
+        <router-link to="/coaches"></router-link>Coaches
+      </p>
+    </base-card>
+  </div>
 </template>
